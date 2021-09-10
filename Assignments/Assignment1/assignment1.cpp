b2809d8ea322cc4a09118af552cdf5b40dc83ace
millionhz <m.waqas.hamza@gmail.com>
2021-09-11 01:56:27 +0500

    assignment 1 fresh added

---
+++ Assignments/Assignment1/assignment1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
+
+/* Suppose you are given the following information */
+// Integer:first_day. This is the day on Jan 1 where 0 is sunday 1 is monday and ... 6 is Saturday
+// Integer: leap_year. leap_year=1 indicates that the given year is leap year and 0 indicates not a leap year
+// Integer: date (between 1 to Max days of the month, both inclusive)
+// Integer: month (between 1 to 12, both inclusive
+/*Your task is to find the day name on the given date and print the output in the following format*/
+// June 10 is Saturday
+/* Valide the input values and print appropriate error messages */
+
+/* Make sure to comply the output format. Month name should be printed instead of numerical equal of the day.
+/* Also day name should be printed instread of numerical equal of the month*/
+
+// Write your code at the specified location in the main function.
+// Do not change the other code.
+// Name the file as firstname_xxxxxxxx. Replace fistname with your actual first name and replace xxxxxxxx
+// with your campuse id
+// upload the file on LMS  without zipping it
+
+int main(int argc, char *argv[])
+{
+	/*****Do not change following code */
+	int first_day = atoi(argv[1]);
+	int leap_year = atoi(argv[2]);
+	int date = atoi(argv[3]);
+	int month = atoi(argv[4]);
+	/***********************************/
+
+	/****Your code starts here***/
+	// No need to take input from the user. First four lines are for user input. So, you
+	// assume that the input values are already in the variables declared in the first four lines.
+
+	/*****Your code ends here*******/
+
+	/*****Do not change following code */
+	return 0;
+	/***********************************/
+}
