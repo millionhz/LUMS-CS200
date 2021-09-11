17aa9b630927bfb12f965152c6321bd8e313347a
millionhz <m.waqas.hamza@gmail.com>
2021-09-12 00:15:08 +0500

    base code done

--- Assignments/Assignment1/Hamza_24100192.cpp
+++ Assignments/Assignment1/Hamza_24100192.cpp
@@ -22,6 +22,62 @@ using namespace std;
 // with your campuse id
 // upload the file on LMS  without zipping it
 
+string day_name(const int x)
+{
+    switch (x)
+    {
+    case 0:
+        return "Sunday";
+    case 1:
+        return "Monday";
+    case 2:
+        return "Tuesday";
+    case 3:
+        return "Wednesday";
+    case 4:
+        return "Thursday";
+    case 5:
+        return "Friday";
+    case 6:
+        return "Saturday";
+    default:
+        return "ERROR";
+    }
+}
+
+string month_name(const int x)
+{
+    switch (x)
+    {
+    case 1:
+        return "January";
+    case 2:
+        return "February";
+    case 3:
+        return "March";
+    case 4:
+        return "April";
+    case 5:
+        return "May";
+    case 6:
+        return "June";
+    case 7:
+        return "July";
+    case 8:
+        return "August";
+    case 9:
+        return "September";
+    case 10:
+        return "October";
+    case 11:
+        return "November";
+    case 12:
+        return "December";
+    default:
+        return "ERROR";
+    }
+}
+
 int main(int argc, char *argv[])
 {
     /*****Do not change following code */
@@ -34,6 +90,42 @@ int main(int argc, char *argv[])
     /****Your code starts here***/
     // No need to take input from the user. First four lines are for user input. So, you
     // assume that the input values are already in the variables declared in the first four lines.
+
+    int day_of_week = first_day;
+
+    const int TOTAL_MONTHS = 12;
+    const int x[TOTAL_MONTHS] = {
+        31,
+        (leap_year) ? 29 : 28,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31,
+    };
+
+    for (int m = 1; m <= month || m > TOTAL_MONTHS; m++)
+    {
+        for (int d = 1; d <= x[m - 1]; d++)
+        {
+
+            cout << d << "-" << m << ": " << day_name(day_of_week) << endl;
+
+            if (m == month && d == date)
+                break;
+
+            day_of_week++;
+            if (day_of_week > 6)
+                day_of_week = 0;
+        }
+    }
+
+    cout << month_name(month) << " " << date << " is " << day_name(day_of_week) << endl;
 
     /*****Your code ends here*******/
 
