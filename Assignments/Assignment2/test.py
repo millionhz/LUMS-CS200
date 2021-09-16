566ee298956897f864782e159f36604f1029509a
millionhz <m.waqas.hamza@gmail.com>
2021-09-16 10:57:53 +0500

    multiprocessing added to test.py

--- Assignments/Assignment2/test.py
+++ Assignments/Assignment2/test.py
@@ -1,28 +1,65 @@
 import datetime
 from subprocess import run
-
-start = datetime.datetime(1900, 12, 31)
-x = datetime.datetime(2001, 1, 1)
-
-z = 0
+from multiprocessing import Process
+from sys import exit
 
 filename = "Hamza_24100192"
 
-run(f"g++ {filename}.cpp -o {filename}.exe", stdout=True)
+# compile
+#run(f"g++ {filename}.cpp -o {filename}.exe", stdout=True)
 
 
-while x != datetime.datetime(2022, 12, 31):
-    command = f"{filename}.exe {x.day} {x.month} {x.year}"
-    c = int(run(command, capture_output=True).stdout.decode("utf-8")[:-2])
-    p = x - start
-    if (c != p.days):
-        print(x)
-        print(c)
-        print(p)
-        break
+def test(year_start, year_end):
+    z = 0  # used for logging
+    start = datetime.datetime(1900, 12, 31)
+    x = datetime.datetime(year_start, 1, 1)
 
-    if x.year != z:
-        print(x.year)
-        z = x.year
+    while x != datetime.datetime(year_end, 12, 31):
+        command = f"{filename}.exe {x.day} {x.month} {x.year}"
+        c = int(run(command, capture_output=True).stdout.decode("utf-8")[:-2])
+        p = x - start
+        if (c != p.days):
+            print(x)
+            print(c)
+            print(p)
+            break
 
-    x = x + datetime.timedelta(days=1)
+        if x.year != z:
+            print(f"Checking {x.year}...")
+            z = x.year
+
+        x = x + datetime.timedelta(days=1)
+
+
+def get_years(end, step):
+    years = []
+    start = 1900
+
+    while start != end:
+        years.append((start+1, start+step))
+        start = years[-1][1]
+
+    return years
+
+
+if __name__ == "__main__":
+
+    years = get_years(2020, 30)
+    process_count = len(years)
+    process = []
+
+    # print(process_count)
+
+    # exit()
+
+    for i in range(process_count):
+        process.append(
+            Process(target=test, args=years[i])
+        )
+        print(process[i].name)
+
+    for i in range(process_count):
+        process[i].start()
+
+    for i in range(process_count):
+        process[i].join()
