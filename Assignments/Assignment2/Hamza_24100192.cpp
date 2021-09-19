f673277218471ec04a6d59ddf8809bd023f37e97
millionhz <m.waqas.hamza@gmail.com>
2021-09-19 14:15:28 +0500

    MatrixTranspose done

--- Assignments/Assignment2/Hamza_24100192.cpp
+++ Assignments/Assignment2/Hamza_24100192.cpp
@@ -83,6 +83,21 @@ int MatrixMultiply(int matrix1[], int r1, int c1, int matrix2[], int r2, int c2,
 // is passed as argument to the function. If the function succeeds return 1 otherwise return -1.
 int MatrixTranspose(int matrix1[], int r1, int c1, int resultant[], int &r2, int &c2)
 {
+
+    for (int i = 0; i < r1 * c1; i++)
+    {
+        if (i == (r1 * c1 - 1))
+        {
+            resultant[r1 * c1 - 1] = matrix1[i];
+        }
+        else
+        {
+            resultant[(r1 * i) % ((r1 * c1) - 1)] = matrix1[i];
+        }
+    }
+
+    r2 = c1;
+    c2 = r1;
     return 0;
 }
 
@@ -104,22 +119,37 @@ int main(int argc, char *argv[])
     // const int SIZE = sizeof(arr) / sizeof(int);
     // cout << FindSingleElement(arr, SIZE) << endl;
 
-    int m1[] = {5, 7, 9, 10, 2, 3, 3, 8, 8, 10, 2, 3, 3, 3, 4, 8};
-    int r1 = 4, c1 = 4;
+    // int m1[] = {5, 7, 9, 10, 2, 3, 3, 8, 8, 10, 2, 3, 3, 3, 4, 8};
+    // int r1 = 4, c1 = 4;
+    // cout << "Matrix 1:" << endl;
+    // printMatrix(m1, r1, c1);
+
+    // int m2[] = {3, 10, 12, 18, 12, 1, 4, 9, 9, 10, 12, 2, 3, 12, 4, 10};
+    // int r2 = 4, c2 = 4;
+    // cout << "Matrix 2:" << endl;
+    // printMatrix(m2, r2, c2);
+
+    // int resultant[r1 * c2] = {0};
+    // int r3, c3;
+    // MatrixMultiply(m1, r1, c1, m2, r2, c2, resultant, r3, c3);
+
+    // cout << "Resultant:" << endl;
+    // printMatrix(resultant, r3, c3);
+    // cout << r3 << "X" << c3 << endl;
+
+    int m1[] = {5, 7, 9, 10, 2, 3};
+    int r1 = 2, c1 = 3;
     cout << "Matrix 1:" << endl;
     printMatrix(m1, r1, c1);
 
-    int m2[] = {3, 10, 12, 18, 12, 1, 4, 9, 9, 10, 12, 2, 3, 12, 4, 10};
-    int r2 = 4, c2 = 4;
-    cout << "Matrix 2:" << endl;
-    printMatrix(m2, r2, c2);
-
-    int resultant[r1 * c2] = {0};
+    int resultant[r1 * c1] = {0};
     int r3, c3;
-    MatrixMultiply(m1, r1, c1, m2, r2, c2, resultant, r3, c3);
+
+    MatrixTranspose(m1, r1, c1, resultant, r3, c3);
 
     cout << "Resultant:" << endl;
     printMatrix(resultant, r3, c3);
     cout << r3 << "X" << c3 << endl;
+
     return 0;
 }
