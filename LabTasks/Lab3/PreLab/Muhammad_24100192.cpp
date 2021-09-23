cf73af95c8f3ee362f86b89ebb87ae0167eb2c06
millionhz <m.waqas.hamza@gmail.com>
2021-09-24 00:25:44 +0500

    PreLab 3 Done

--- LabTasks/Lab3/PreLab/Muhammad_24100192.cpp
+++ LabTasks/Lab3/PreLab/Muhammad_24100192.cpp
@@ -11,6 +11,12 @@ public:
     MyString(char *ptr);
     ~MyString();
     int GetLength();
+    void SetString(char *ptr);
+    bool IsEqualTo(char *ptr);
+
+private:
+    void _SetString(char *ptr);
+    int _CalculateLength(char *ptr);
 };
 
 MyString::MyString()
@@ -21,11 +27,12 @@ MyString::MyString()
 
 MyString::MyString(char *ptr)
 {
-    // calculate length;
-    while (*(ptr + length) != '\0')
-    {
-        length++;
-    }
+    _SetString(ptr);
+}
+
+void MyString::_SetString(char *ptr)
+{
+    length = _CalculateLength(ptr);
 
     str = new char[length];
 
@@ -36,9 +43,49 @@ MyString::MyString(char *ptr)
     *(str + length) = '\0';
 }
 
+int MyString::_CalculateLength(char *ptr)
+{
+    int len = 0;
+    while (*(ptr + len) != '\0')
+    {
+        len++;
+    }
+    return len;
+}
+
 int MyString::GetLength()
 {
     return length;
+}
+
+void MyString::SetString(char *ptr)
+{
+    delete[] str;
+    str = NULL;
+
+    _SetString(ptr);
+}
+
+bool MyString::IsEqualTo(char *ptr)
+{
+    int length_ptr = _CalculateLength(ptr);
+
+    if (length_ptr == length)
+    {
+        for (int i; i < length; i++)
+        {
+            if (*(ptr + i) != *(str + i))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 MyString::~MyString()
@@ -54,6 +101,13 @@ int main()
     MyString s(str);
     cout << s.str << endl;
     cout << s.GetLength() << endl;
+    s.SetString((char *)"CS200 Is Kinda");
+    cout << s.str << endl;
+    cout << s.GetLength() << endl;
+
+    cout << s.IsEqualTo(str) << endl;
+    s.SetString(str);
+    cout << s.IsEqualTo(str) << endl;
 
     return 0;
 }
