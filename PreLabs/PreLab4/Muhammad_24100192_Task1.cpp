a51771c903f31d57756b649a670d87aca729a4cc
millionhz <m.waqas.hamza@gmail.com>
2021-09-29 17:51:58 +0500

    Union done; pointer deref changed to array indexing

--- PreLabs/PreLab4/Muhammad_24100192_Task1.cpp
+++ PreLabs/PreLab4/Muhammad_24100192_Task1.cpp
@@ -23,7 +23,7 @@ public:
 
             for (int s = 0; s < length; s++)
             {
-                if (*(_arr + i) == *(set + s))
+                if (_arr[i] == set[s])
                 {
                     already_present = true;
                     break;
@@ -32,7 +32,7 @@ public:
 
             if (!already_present)
             {
-                *(set + length) = *(_arr + i);
+                set[length] = _arr[i];
                 length++;
             }
         }
@@ -44,17 +44,32 @@ public:
         set = NULL;
     }
 
-    // Set Union(Set s1)
-    // {
-    //     return Set();
-    // }
+    Set Union(Set other_set)
+    {
+        int arr[this->length + other_set.length];
+        int index_arr = 0;
+
+        for (int i = 0; i < this->length; i++)
+        {
+            arr[index_arr] = this->set[i];
+            index_arr++;
+        }
+
+        for (int i = 0; i < other_set.length; i++)
+        {
+            arr[index_arr] = other_set.set[i];
+            index_arr++;
+        }
+
+        return Set(arr, index_arr);
+    }
 
     void printSet()
     {
         cout << "{";
         for (int i = 0; i < length; i++)
         {
-            cout << *(set + i) << ", ";
+            cout << set[i] << ", ";
         }
         cout << "\b\b}" << endl;
     }
@@ -66,10 +81,16 @@ private:
 
 int main()
 {
-    int arr[] = {1, 2, 1, 3, 5, 1, -1};
-    Set s1 = Set(arr, sizeof(arr) / sizeof(int));
-
+    int arr1[] = {1, 2, 1, 3, 5, 1, -1};
+    Set s1 = Set(arr1, sizeof(arr1) / sizeof(int));
     s1.printSet();
+
+    int arr2[] = {-1, 2, 1, -3, 55, -5, 1, -1, 7, 11, 11, 14};
+    Set s2 = Set(arr2, sizeof(arr2) / sizeof(int));
+    s2.printSet();
+
+    Set resultant = s1.Union(s2);
+    resultant.printSet();
 
     return 0;
 }
