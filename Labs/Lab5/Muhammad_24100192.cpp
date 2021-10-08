dfb3e01ed51be89d6f2266d4559feffb9a4de1d1
millionhz <m.waqas.hamza@gmail.com>
2021-10-09 04:18:22 +0500

    if no prevNode, then pointer at head

--- Labs/Lab5/Muhammad_24100192.cpp
+++ Labs/Lab5/Muhammad_24100192.cpp
@@ -80,6 +80,7 @@ public:
         new_node->setData(data);
 
         Node *current_node = getNode(index);
+
         new_node->setNextPointer(current_node);
 
         Node *prev_node = getNode(index - 1);
@@ -88,8 +89,7 @@ public:
         {
             prev_node->setNextPointer(new_node);
         }
-
-        if (head == NULL || index == 0)
+        else
         {
             head = new_node;
         }
@@ -116,8 +116,7 @@ public:
         {
             prev_node->setNextPointer(next_node);
         }
-
-        if (index == 0)
+        else
         {
             head = next_node;
         }
