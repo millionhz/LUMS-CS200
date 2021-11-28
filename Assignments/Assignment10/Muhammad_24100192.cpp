6b7af6a0f2635e34ef0a801b97ffa5b06e74019f
millionhz <m.waqas.hamza@gmail.com>
2021-11-29 03:18:10 +0500

    Assignment10 init

---
+++ Assignments/Assignment10/Muhammad_24100192.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Teacher
+{
+private:
+protected:
+public:
+};
+
+class Student
+{
+private:
+protected:
+public:
+};
+
+int main()
+{
+    return 0;
+}
