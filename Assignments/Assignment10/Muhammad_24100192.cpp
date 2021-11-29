38bb902b08a53239638333beb8e49ded57a7ef96
millionhz <m.waqas.hamza@gmail.com>
2021-11-29 19:33:35 +0500

    data members done

--- Assignments/Assignment10/Muhammad_24100192.cpp
+++ Assignments/Assignment10/Muhammad_24100192.cpp
@@ -2,18 +2,52 @@
 
 using namespace std;
 
+class Student;
+
 class Teacher
 {
 private:
+    string name;
+    int num_of_students;
+    static Student *students;
+
 protected:
 public:
+    Teacher(string &name)
+    {
+        this->name = name;
+        num_of_students = 0;
+        students = nullptr;
+    }
+
+    string getName()
+    {
+        return name;
+    }
 };
 
 class Student
 {
 private:
+    string name;
+    int num_of_teachers;
+    static Teacher *teachers;
+
 protected:
 public:
+    Student() {}
+
+    Student(string &name)
+    {
+        this->name = name;
+        num_of_teachers = 0;
+        teachers = nullptr;
+    }
+
+    string getName()
+    {
+        return name;
+    }
 };
 
 int main()
